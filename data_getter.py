c02f616c324730fb4ed5ad6b6c05a8e6f93b4072
Kushal Vajrala <vajrala.kushal@gmail.com>
2020-06-07 14:46:08 -0500

    Used class and method based program instead of a script

--- data_getter.py
+++ data_getter.py
@@ -1,4 +1,26 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver_') #make sure that you have the chrome webdriver installed and placed in path
-driver.get("https://simple.wikipedia.org")
+#when you run the program in your terminal, make sure that you use python3 -i data_getter.py
+#this will make sure that the website doesn't immediately
+class WikiBot:
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {
+            "download.default_directory": "~/ProgrammingProjects/SummerHackathon/PDF_Files", #Change default directory for downloads
+            "download.prompt_for_download": False, #To auto download the file
+            "download.directory_upgrade": True,
+            "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
+        })
+        driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver_', options = options) #make sure that you have the chrome webdriver installed and placed in path
+        driver.get("https://simple.wikipedia.org")
+
+    def get_list_articles(self):
+        print("hello")
+        
+    
+
+
+bot = WikiBot()
+
+
