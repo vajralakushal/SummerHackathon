7d695497fbf8a7e426103c1d76653f1f65774c6b
Ayaan Mahimwala <ayaanhaider@gmail.com>
2020-06-07 14:46:53 -0500

    Change webdrive path

--- data_getter.py
+++ data_getter.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver_') #make sure that you have the chrome webdriver installed and placed in path
+driver = webdriver.Chrome() #make sure that you have the chrome webdriver installed and placed in path
 driver.get("https://simple.wikipedia.org")
