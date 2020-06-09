a8ae5631c5e2fdc1d01bd7f5f72a13f2c5abd8f9
Kushal Vajrala <vajrala.kushal@gmail.com>
2020-06-09 12:10:46 -0500

    fixed the chromedriver_ that was part of my code but not necessary; adjusted my path as well

--- data_getter.py
+++ data_getter.py
@@ -8,12 +8,12 @@ class WikiBot:
     def __init__(self):
         options = webdriver.ChromeOptions()
         options.add_experimental_option('prefs', {
-            "download.default_directory": "~/GoogleDrive/PDF-Files-Hackathon", #Change default directory for downloads
+            "download.default_directory": "~/GoogleDrive/PDF-Files-Hackathon", #Change default directory for downloads; this will only work for Kushal so far, working on putting a folder and sharing it with the rest of you guys
             "download.prompt_for_download": False, #To auto download the file
             "download.directory_upgrade": True,
             "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
         })
-        driver = webdriver.Chrome(options = options) #make sure that you have the chrome webdriver installed and placed in path
+        driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver',options = options) #make sure that you have the chrome webdriver installed and placed in path
         driver.get("https://simple.wikipedia.org")
 
     def get_list_articles(self):
