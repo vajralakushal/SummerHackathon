884bc8906208fa8a63a11d427fa6ee83ba22489b
Kushal Vajrala <vajrala.kushal@gmail.com>
2020-06-10 21:53:50 -0500

    added wikipedia-api to requirements, going to test wikipedia api on a different branch

--- data_getter.py
+++ data_getter.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
-
+import bs4
+import requests
 #when you run the program in your terminal, make sure that you use python3 -i data_getter.py
 #this will make sure that the website doesn't immediately
-#executable_path='/usr/local/bin/chromedriver_'
+#executable_path='/usr/local/bin/chromedriver'
 class WikiBot:
-
     def __init__(self):
         options = webdriver.ChromeOptions()
         options.add_experimental_option('prefs', {
@@ -13,12 +13,15 @@ class WikiBot:
             "download.directory_upgrade": True,
             "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
         })
-        options.add_experimental_option("detach", True)
+        options.add_experimental_option("detach", True) #if you don't want to keep your chrome windows open, comment out this line.
         driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver',options = options) #make sure that you have the chrome webdriver installed and placed in path
-        driver.get("https://simple.wikipedia.org")
+        driver.get("https://simple.wikipedia.org/w/index.php?title=Special:AllPages&hideredirects=1")
 
-    def get_list_articles(self):
+    def get_article_intro(self):
         print("hello how are you doing")
+    
+    def put_intro_into_txt(self):
+        print("mic testing")
 
     
 
